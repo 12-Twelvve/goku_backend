439a3ae1e6a88c417991cdd90facf67e3a72e991
LEGION <koobear99@gamil.com>
2023-09-23 16:27:13 +0545

    auth handler added

--- src/handler/auth.handler.ts
+++ src/handler/auth.handler.ts
@@ -13,23 +13,18 @@ authHandler.post(
       // console.log(req.body)
       const { username, password } = req.body
       // check if user already exist
-      // Validate if user exist in our database
       const existingUser = await getUserByUsername(username)
 
       if (existingUser) {
         return res.status(409).send('User Already Exist. Please Login')
       }
-
       //Encrypt user password
       const encryptedPassword = await bcrypt.hash(password, 10)
-
       // Create user in our database
       const newUser = await createUser({
         username,
         password: encryptedPassword,
       })
-      // Create token
-      // const token = signJwt({ username, _id: newUser._id })
       const token = 'random'
       // return new user with token
       res.status(200).json({ _id: newUser._id, token })
@@ -52,8 +47,6 @@ authHandler.post(
       const user = await getUserByUsername(username)
       if (user && (await bcrypt.compare(password, user.password))) {
         // Create token
-        // const token = signJwt({ username, _id: user._id })
-        // user
         const token = "random"
         return res.status(200).json({ _id: user._id, token })
       }
